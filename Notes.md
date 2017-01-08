4ad1d755d4eda939b4c72673287d8944b4bc519b
Yuchao Han <yuchao@indeed.com>
2017-01-07 21:28:37 -0800

    use () in return & wrap multi node elements into one element

--- Notes.md
+++ Notes.md
@@ -7,3 +7,4 @@
     + 不同点: 因为在react中`class`是一个*reserved name*,所以要用`className`来代替
     + all JSX compiled into javascript
         * 实际上会使用`React.createElement`函数来创建
+        * 这就决定了只能return 唯一一个element
--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import React from 'react';
 //creat component in different ways:
 
 //#1  extends React.Component
-//class App extends React.Component {
-//  render() {
-//    //using JSX, which is a HTML like syntax
-//    //all JSX compiled into javascript(using React.createElement)
-//    return <h1>hello worlds</h1>
-//    //这句话等价于  return React.createElement('h1', null, `hello world`)
-//  }
-//}
+class App extends React.Component {
+  render() {
+    //using JSX, which is a HTML like syntax
+    //all JSX compiled into javascript(using React.createElement)
+    return (
+        <div>
+          <h1>hello worlds</h1>
+          <h2>this is yuchao</h2>
+        </div>
+    )
+    //这句话等价于  return React.createElement('h1', null, `hello world`)
+      //注意:JSX只能return 唯一一个node element,也就是说不能`return <h1>hello</h1> <h2>world</h2>`
+  }
+}
 
-//#2  define a stateless function, which simply returns JSX
-const App = () => <h1>hello world</h1>
+////#2  define a stateless function, which simply returns JSX
+//const App = () => <h1>hello world</h1>
 
 //两者区别: class component可以包含state,但是function component是无状态的
 
