2c06bc42b4b6b494c2e0d56f3f2c65639c0b2047
Yuchao Han <yuchao@indeed.com>
2017-01-07 21:21:28 -0800

    two different ways to create react component

---
+++ Notes.md
@@ -0,0 +1,9 @@
+- `import React from 'react';`
+    + library to enable us build react component
+- `import ReactDOM from 'react-dom';`
+    + enable us to play react component in the context of DOM
+- JSX
+    + a HTML like syntax
+    + 不同点: 因为在react中`class`是一个*reserved name*,所以要用`className`来代替
+    + all JSX compiled into javascript
+        * 实际上会使用`React.createElement`函数来创建
--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
-const App = () => <h1>Hi</h1>
+//creat component in different ways:
+
+//#1  extends React.Component
+//class App extends React.Component {
+//  render() {
+//    //using JSX, which is a HTML like syntax
+//    //all JSX compiled into javascript(using React.createElement)
+//    return <h1>hello worlds</h1>
+//    //这句话等价于  return React.createElement('h1', null, `hello world`)
+//  }
+//}
+
+//#2  define a stateless function, which simply returns JSX
+const App = () => <h1>hello world</h1>
+
+//两者区别: class component可以包含state,但是function component是无状态的
 
 export default App
