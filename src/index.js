beb33f8741df8ba5ec96be262ca8398f0e96c4a8
Yuchao Han <yuchao@indeed.com>
2017-01-07 18:38:42 -0800

    create react app using   create-react-app <project-directory>
    For example:
      create-react-app my-react-app
    
    Run create-react-app --help to see all options. npm command

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';//library to enable us build react component
+import ReactDOM from 'react-dom';//enable us to play react component in the context of DOM
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
